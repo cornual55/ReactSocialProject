1f7a056426fd7b4bca91a96cd4394c9e9fa8a587
Anatoly <tolyamelnik020406@gmail.com>
2025-08-25 22:08:36 +0800

    navbar complete

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -37,12 +37,12 @@ const Navbar = () => {
     const theme = useTheme();
     const neutralLight = theme.palette.neutral.light;
     const dark = theme.palette.neutral.dark;
-    const background = theme.palette.background.default;
+    const backgroundColor = theme.palette.background.default;
     const primaryLight = theme.palette.primary.light;
     const alt = theme.palette.background.alt;
 
-    const fullName = `${user.firstName} ${user.lastName}`;
-
+    //const fullName = `${user.firstName} ${user.lastName}`;
+    const fullName = "Test User";
 
     return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
